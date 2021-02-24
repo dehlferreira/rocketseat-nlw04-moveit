e695f14fc1adba3ac3abf1e80bf1d356f54f1e22
André Luiz Ferreira <andre.ferreira@labsit.io>
2021-02-23 23:43:43 -0300

    feat(project): adding image level

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -6,7 +6,10 @@ export function Profile() {
       <img src="https://github.com/dehlferreira.png" alt="André Luiz Ferreira"/>
       <div>
         <strong>André Luiz Ferreira</strong>
-        <p>Level 1</p>
+        <p>
+          <img src="icons/level.svg" alt="Level"/>
+          Level 1
+        </p>
       </div>
     </div>
   );
