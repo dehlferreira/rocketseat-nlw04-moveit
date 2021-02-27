ae4f3b17352eaf71dbd8e728732735a835f7e0a9
André Luiz Ferreira <andre.ferreira@labsit.io>
2021-02-27 00:51:30 -0300

    feat(login page): creating login page

---
+++ src/pages/login.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/pages/Login.module.css';
+
+export default function Login() {
+  return (
+    <div className={styles.container}>
+      <section>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+      </section>
+    </div>
+  )
+}
